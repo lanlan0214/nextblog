514d2606de0c4617b59d66698fd2d42e0a603127
frank <User@DESKTOP-OUEGC93.(none)>
2023-09-18 17:56:28 +0800

    202309181800-cat

--- src/components/category/CategoryList.jsx
+++ src/components/category/CategoryList.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import styles from "./categoryList.module.css"
-import Link from 'next/link'
-import Image from 'next/image'
-import Pagination from '../pagination/Pagination'
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const getData = async () => {
-  const res = await fetch(`http://localhost:3000/api/categories`, {
+  const res = await fetch("http://localhost:3000/api/categories", {
     cache: "no-store",
-  })
+  });
 
   if (!res.ok) {
-    throw new Error("Failed")
+    throw new Error("Failed");
   }
 
-  return res.json()
-}
+  return res.json();
+};
 
-const CategoryList = async ({ page, cat }) => {
-
-  const { posts, count } = await getData(page, cat)
-
-  const POST_PER_PAGE = 2
-
-  const hasPrev = POST_PER_PAGE * (page - 1) > 0
-  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count
-
-  const data = await getData()
+const CategoryList = async () => {
+  const data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        {data?.map(item => (
-          <Link href={`/blog?cat=${item.slug}`} className={`${styles.category} ${styles[item.slug]}`} key={item._id}>
-            {item.img && (<Image src={item.img} alt='' width={32} height={32} className={styles.image} />)}
+        {data?.map((item) => (
+          <Link
+            href={`/blog?cat=${item.slug}`}
+            className={`${styles.category} ${styles[item.slug]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
             {item.title}
           </Link>
         ))}
-        <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
